9352eba91ba5aba654510c9f88b82bf24b971711
avishek0769 <avishekadhikary42@gmail.com>
2025-08-23 22:52:34 +0530

    Configure Middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,21 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"])
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"])
 
 export default clerkMiddleware(async (auth, req) => {
-    if (!isPublicRoute(req)) {
-        await auth.protect()
+    const { userId } = await auth()
+    const isAccessingDashboard = req.nextUrl.pathname == "/dashboard"
+
+    if (userId && isPublicRoute(req) && !isAccessingDashboard) {
+        return NextResponse.redirect(new URL("/home", req.url))
     }
+    if (!userId && !isPublicRoute(req)) {
+        return NextResponse.redirect(new URL("/sign-in", req.url))
+    }
+    return NextResponse.next()
 })
 
 export const config = {
-    matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
-    ],
-}
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
